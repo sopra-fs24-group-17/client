3edb504726de7e297aa939e7a0ce87268a363e0d
Liam <liam.tessendorf@uzh.ch>
2024-04-16 08:30:08 +0200

    issue #65 - closes issue #65 - added cards

--- src/components/ui/CardComponent.tsx
+++ src/components/ui/CardComponent.tsx
@@ -3,30 +3,26 @@ import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import { motion } from "framer-motion";
 
 interface CardProps {
-  text: string;
-  description: string;
-  image: string;
-  onClick?: () => void;
+    text: string;
+    image: string;
+    onClick?: () => void;
 }
 
 const CardComponent: React.FC<CardProps> = ({
-  text,
-  description,
-  image,
-  onClick,
+    text,
+    image,
+    onClick,
 }) => {
-  return (
-    <Card sx={{ width: 125, height: 175 }} onClick={onClick}>
-      <CardContent>
-        <Typography variant="h5" component="div">
-          {text}
-          {image}
-          {description}
-        </Typography>
-        {/* Add more content here as needed */}
-      </CardContent>
-    </Card>
-  );
+    return (
+        <Card sx={{ width: 125, height: 175 }} onClick={onClick}>
+            <CardMedia
+                component="img"
+                height="175"  // Adjust height as needed
+                image={image}
+                alt={text}
+            />
+        </Card>
+    );
 };
 
 export default CardComponent;
