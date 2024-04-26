bccfc1b769036b2d4989df14e096f742618e9b06
Liam <liam.tessendorf@uzh.ch>
2024-04-26 13:24:46 +0200

    issue #68 - closes #68 #78 - basically all gamelogic implemented

--- src/components/views/Game.tsx
+++ src/components/views/Game.tsx
@@ -11,47 +11,58 @@ import { playCard } from "components/game/playCard";
 const Game = () => {
   const gameId = localStorage.getItem("gameId");
   const userId = localStorage.getItem("id");
-  const [closedDeck, setClosedDeck] = useState(cardTypes); // The deck that players will draw from
-  const [openDeck, setOpenDeck] = useState([]); // The deck that shows the played cards
-  const [playerHand, setPlayerHand] = useState([]); // The current player's hand
+  const [closedDeck, setClosedDeck] = useState(cardTypes);
+  const [openDeck, setOpenDeck] = useState([]);
+  const [playerHand, setPlayerHand] = useState([]);
   const [playerTurn, setPlayerTurn] = useState(false);
   const navigate = useNavigate();
   const numberOfPlayers = 2;
 
-  // Stomp subscription reference
   const subscriptionRef = useRef(null);
 
   const handleIncomingMessage = useCallback((message) => {
-    // Parse the incoming message and update state accordingly
     const gameState = JSON.parse(message.body);
-    // Check if the type of the message is 'cards'
     if (gameState.type === 'cards') {
-      // Update the player's hand with the received and enhanced cards
       setPlayerHand((prevHand) => [...prevHand, ...enhanceCardDetails(gameState.cards)]);
       console.log("Received and enhanced cards:", enhanceCardDetails(gameState.cards));
     } else if (gameState.type === "startTurn") {
       setPlayerTurn(true);
     } else if (gameState.type === "endTurn") {
       setPlayerTurn(false);
-    }
-    else if (gameState.type === "peekIntoDeck") {
+    } else if (gameState.type === "peekIntoDeck") {
       peekIntoDeck(gameState.cards);
+    } else if (gameState.type === "cardStolen") {
+      cardStolen(gameState.cards);
+    } else if (gameState.type === "explosion") {
+      handleExplosion(gameState.terminatingUser);
+    } else if (gameState.type === "defuseCard") {
+      handleDefuseCard();
     } else if (gameState.type === "gameState") {
       if (gameState.topCardInternalCode) {
         handleOpenDeck(gameState.topCardInternalCode);
-      } else {
-
       }
     } else if (gameState.type === "endGame") {
       alert("Game Over! The winner is: " + gameState.winningUser);
       navigate("/dashboard/join-game");
     }
-
   }, []);
 
   const peekIntoDeck = (cards) => {
     const cardsString = cards.map(card => `${card.internalCode}`).join('\n');
     alert(`The next 3 cards are:\n${cardsString}`);
+  };
+
+  const cardStolen = (cards) => {
+    const stolenCard = cards[0].internalCode;
+    setPlayerHand((prevHand) => prevHand.filter((card) => card.name !== stolenCard));
+  };
+
+  const handleExplosion = (userName) => {
+    alert(`Player ${userName} drew an Exploding Chicken! Do they have a Defuse card?`);
+  };
+
+  const handleDefuseCard = () => {
+    setPlayerHand((prevHand) => prevHand.filter((card) => card.name !== "defuse"));
   };
 
   const handleOpenDeck = (topCardInternalCode) => {
@@ -61,31 +72,23 @@ const Game = () => {
 
   const enhanceCardDetails = (cards) => {
     return cards.map((card) => {
-      // Find the matching card type by internalCode
       const cardType = cardTypes.find((type) => type.name === card.internalCode);
       if (cardType) {
-        // Return a new object combining the card info from the server and the cardType details
         return { ...card, ...cardType };
       }
-      return card; // Return the original card if no match is found
+      return card;
     });
   };
 
   useEffect(() => {
     let stompClient = null;
-    // Establish the WebSocket connection and subscribe to the channel
     connectWebSocket().then(client => {
       stompClient = client;
       subscriptionRef.current = subscribeToChannel(`/game/${gameId}/${userId}`, handleIncomingMessage);
       subscriptionRef.current = subscribeToChannel(`/game/${gameId}`, handleIncomingMessage);
+      sendMessage(`/app/start/${gameId}`, {});
     });
   }, []);
-
-  const startGame = () => {
-    // Send a message to the server that the game is starting
-    sendMessage(`/app/start/${gameId}`, {});
-  };
-
 
   return (
     <Grid container spacing={2} style={{ height: "100vh", padding: "20px" }}>
@@ -94,7 +97,6 @@ const Game = () => {
           {`Your turn: ${playerTurn}`}
         </Typography>
       </Grid>
-      <Button onClick={startGame}>Start Game</Button>
       {numberOfPlayers <= 2 && (
         <Grid
           item
@@ -115,7 +117,7 @@ const Game = () => {
                   key={card.id}
                   className="card"
                   style={{
-                    transform: `translateX(${index * 2}px) translateY(${index * -2}px)`, // Offset each card slightly
+                    transform: `translateX(${index * 2}px) translateY(${index * -2}px)`,
                   }}
                 >
                   <CardComponent
@@ -150,7 +152,7 @@ const Game = () => {
                     key={card.id}
                     className="card"
                     style={{
-                      transform: `translateX(${index * 2}px) translateY(${index * -2}px)`, // Offset each card slightly
+                      transform: `translateX(${index * 2}px) translateY(${index * -2}px)`,
                     }}
                   >
                     <CardComponent
@@ -182,7 +184,7 @@ const Game = () => {
                     key={card.id}
                     className="card"
                     style={{
-                      transform: `translateX(${index * 2}px) translateY(${index * -2}px)`, // Offset each card slightly
+                      transform: `translateX(${index * 2}px) translateY(${index * -2}px)`,
                     }}
                   >
                     <CardComponent
@@ -215,7 +217,7 @@ const Game = () => {
                   key={card.id}
                   className="card"
                   style={{
-                    transform: `translateX(${index * 2}px) translateY(${index * -2}px)`, // Offset each card slightly
+                    transform: `translateX(${index * 2}px) translateY(${index * -2}px)`,
                   }}
                 >
                   <CardComponent
@@ -235,14 +237,14 @@ const Game = () => {
           {closedDeck.slice(0, 5).map(
             (
               card,
-              index // Display only the top 5 cards for visual effect
+              index
             ) => (
               <div
                 key={card.id}
                 className="card"
                 style={{
-                  zIndex: closedDeck.length - index, // Ensure the top card is clickable
-                  transform: `translateX(${index * 2}px) translateY(${index * -2}px)`, // Offset each card slightly
+                  zIndex: closedDeck.length - index,
+                  transform: `translateX(${index * 2}px) translateY(${index * -2}px)`, 
                 }}
               >
                 <CardComponent
@@ -265,7 +267,7 @@ const Game = () => {
                 key={card.id}
                 className="card"
                 style={{
-                  transform: `translateX(${index * 2}px) translateY(${index * -2}px)`, // Offset each card slightly
+                  transform: `translateX(${index * 2}px) translateY(${index * -2}px)`,
                 }}
               >
                 <CardComponent
@@ -296,7 +298,7 @@ const Game = () => {
                   key={card.id}
                   className="card"
                   style={{
-                    transform: `translateX(${index * 2}px) translateY(${index * -2}px)`, // Offset each card slightly
+                    transform: `translateX(${index * 2}px) translateY(${index * -2}px)`,
                   }}
                 >
                   <CardComponent
